be39ae3de920e00c15e2828485b161b413d08179
MelissaCanu <148777539+MelissaCanu@users.noreply.github.com>
2024-03-01 13:50:55 +0100

    TotVerbaliPerTrasgressore action in HomeController complete

--- MVCExam1/Controllers/HomeController.cs
+++ MVCExam1/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MVCExam1.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +15,54 @@ namespace MVCExam1.Controllers
         {
             return View();
         }
+
+        //action per mostrare il totale dei verbali per trasgressore
+        //creo model TrasgressoreVerbali
+        public ActionResult TotVerbaliPerTrasgressore()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString.ToString();
+            SqlConnection connection = new SqlConnection(connectionString);
+            List<TrasgressoreVerbali> listTrasgressoreVerbali = new List<TrasgressoreVerbali>();
+
+            try
+            {
+                //query divisa su più righe per leggibilità 
+                //seleziono cognome, nome, conteggio dei verbali per trasgressore
+                //da tabella Anagrafica e Verbale e li raggruppo per cognome e nome
+                string query = "SELECT A.IDanagrafica, A.Cognome, A.Nome, COUNT(V.IDanagrafica) AS TotVerbali " +
+                              "FROM Anagrafica A JOIN Verbale V ON A.IDanagrafica = V.IDanagrafica " +
+                              "GROUP BY A.IDanagrafica, A.Cognome, A.Nome";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    TrasgressoreVerbali trasgressoreVerbali = new TrasgressoreVerbali();
+                    trasgressoreVerbali.IDanagrafica = Convert.ToInt32(reader["IDanagrafica"]);
+                    trasgressoreVerbali.Cognome = reader["Cognome"].ToString();
+                    trasgressoreVerbali.Nome = reader["Nome"].ToString();
+                    trasgressoreVerbali.TotVerbali = Convert.ToInt32(reader["TotVerbali"]);
+                    listTrasgressoreVerbali.Add(trasgressoreVerbali);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            
+            return View(listTrasgressoreVerbali);
+        }
+
+
+
+
+
 
         public ActionResult About()
         {
---
+++ MVCExam1/Models/TrasgressoreVerbali.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MVCExam1.Models
+{
+    public class TrasgressoreVerbali
+    {
+        public string Cognome { get; set; }
+        public string Nome { get; set; }
+        public int TotVerbali { get; set; }
+
+        public int IDanagrafica { get; set; }
+    }
+}
