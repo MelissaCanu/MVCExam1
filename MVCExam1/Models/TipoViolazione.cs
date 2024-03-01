05706cc743ad5ab02344da4bfb708963981a6cd5
MelissaCanu <148777539+MelissaCanu@users.noreply.github.com>
2024-03-01 10:25:15 +0100

    models Anagrafica, TipoViolazione, Verbale created

---
+++ MVCExam1/Models/TipoViolazione.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCExam1.Models
+{
+    public class TipoViolazione
+    {
+        public int IDviolazione { get; set; }
+        public string Descrizione { get; set; }
+        public bool Contestabile { get; set; }
+    }
+}
